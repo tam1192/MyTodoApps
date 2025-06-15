3b850b488c6bbf4c0c0ad77a285821e92a1d7062
nikki9750 <76221172+tam1192@users.noreply.github.com>
2025-06-15 17:36:31 +0000

    環境毎に設定をつけた

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,9 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
+  logLevel: "info",
+  runtimeConfig: {
+    databaseName: "main",
+  },
   future: {
     compatibilityVersion: 4,
   },
@@ -11,5 +15,17 @@ export default defineNuxtConfig({
   compatibilityDate: '2025-05-15',
   devtools: { enabled: true },
   modules: ['@nuxt/ui', '@prisma/nuxt', '@nuxt/test-utils'],
-  css: ['assets/css/main.css']
+  css: ['assets/css/main.css'],
+  $test: { 
+    logLevel: "silent",
+    runtimeConfig: {
+      databaseName: "test",
+    },
+  },
+  $development: { 
+    logLevel: "verbose",
+    runtimeConfig: {
+      databaseName: "dev",
+    },
+  },
 })
